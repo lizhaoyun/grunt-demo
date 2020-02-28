846145c027c1fdc976f2bf96f617b29ccae68442
lizhaoyun <2571770878@qq.com>
2020-02-28 09:44:46 +0800

    apitest

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,28 +2,23 @@
 /*global module:true*/
 module.exports=function(grunt){
     grunt.initConfig({
-        mocha_istanbul: {
-            coverage: {
-                src: 'test', // a folder works nicely
-                options: {
-                    mask: '*.spec.js'
-                }
+        run:{
+            options:{wait:false},
+            args:'./app.js'
+        },
+        mochacli: {
+            all:{
+                src:['test/*.js']
             },
-            coveralls: {
-                src: ['test', 'testSpecial', 'testUnique'], // multiple folders also works
-                options: {
-                    coverage:true, // this will make the grunt.event.on('coverage') event listener to be triggered
-                    check: {
-                        lines: 75,
-                        statements: 75
-                    },
-                    root: './lib', // define where the cover task should consider the root of libraries that are covered by tests
-                    reportFormats: ['cobertura','lcovonly']
-                }
+            options:{
+                repottor:'spec',
+                bail:true
             }
         }
     });
-    grunt.loadNpmTasks('grunt-mocha-istanbul');
-    grunt.registerTask('coveralls', ['mocha_istanbul:coveralls']);
-    grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
+    grunt.loadNpmTasks('grunt-mocha-cli');
+    grunt.loadNpmTasks('grunt-run');
+
+    // grunt.registerTask('coveralls', ['mocha_istanbul:coveralls']);
+    grunt.registerTask('default', ['run','mochacli','stop:api']);
 }
