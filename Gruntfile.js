d3cff13635476ea4e89458e94513ce718b88319e
lizhaoyun <2571770878@qq.com>
2020-03-13 11:54:50 +0800

    实现mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,12 @@
-
-// /*global module:true*/
-// module.exports=function(grunt){
-//     grunt.initConfig({
-//         mocha_istanbul: {
-//             coverage: {
-//                 src: 'test', // a folder works nicely
-//                 options: {
-//                     mask: '*.spec.js'
-//                 }
-//             },
-//             coveralls: {
-//                 src: ['test', 'testSpecial', 'testUnique'], // multiple folders also works
-//                 options: {
-//                     coverage:true, // this will make the grunt.event.on('coverage') event listener to be triggered
-//                     check: {
-//                         lines: 75,
-//                         statements: 75
-//                     },
-//                     root: './lib', // define where the cover task should consider the root of libraries that are covered by tests
-//                     reportFormats: ['cobertura','lcovonly']
-//                 }
-//             }
-//         }
-//     });
-//     grunt.loadNpmTasks('grunt-mocha-istanbul');
-//     grunt.registerTask('coveralls', ['mocha_istanbul:coveralls']);
-//     grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
-// }
 module.exports = function(grunt) {
     grunt.initConfig({
+        mochacli: {
+            options: {
+              reporter: 'spec',
+              bail: true
+            },
+            all: ['test/*.js']
+        },
         mocha_istanbul: {
             coverage: {
                 src: "test"
@@ -50,6 +28,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-mocha-cli");
     grunt.loadNpmTasks("grunt-mocha-istanbul");
 
+    grunt.registerTask('default',['mochacli']);
     grunt.registerTask("cover", ["mocha_istanbul"]);
     grunt.registerTask("check-cover", ["istanbul_check_coverage"])
 }
---
+++ app.js
@@ -0,0 +1,3 @@
+const add = require('./calc');
+
+console.log(`30 + 40 = ${add(30, 40)}`);
