1e31e8111c7d005d77962864e2ccc339e166f6f9
lizhaoyun <2571770878@qq.com>
2020-04-07 20:38:51 +0800

    0

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,36 @@
 /*global module:true*/
 module.exports=function(grunt){
     grunt.initConfig({
-        run:{
-            options:{wait:false},
-            args:'./app.js'
-        },
-        mochacli: {
-            all:{
-                src:['test/*.js']
+        // run:{
+        //     options:{wait:false},
+        //     args:'./app.js'
+        // },
+        // mochacli: {
+        //     all:{
+        //         src:['test/*.js']
+        //     },
+        //     options:{
+        //         repottor:'spec',
+        //         bail:true
+        //     }
+        // }
+        mocha: {
+            test: {
+              src: ['test/index.html'],
             },
-            options:{
-                repottor:'spec',
-                bail:true
+            options: {
+              run: true,
+              reporter: 'Dot'
             }
-        }
+          }
     });
-    grunt.loadNpmTasks('grunt-mocha-cli');
-    grunt.loadNpmTasks('grunt-run');
+    // grunt.loadNpmTasks('grunt-mocha-cli');
+    // grunt.loadNpmTasks('grunt-run');
 
     // grunt.registerTask('coveralls', ['mocha_istanbul:coveralls']);
-    grunt.registerTask('default', ['run','mochacli','stop:api']);
+    // grunt.registerTask('default', ['run','mochacli','stop:api']);
+
+    grunt.loadNpmTasks('grunt-mocha');
+
+    grunt.registerTask('default', ['mocha']);
 }
